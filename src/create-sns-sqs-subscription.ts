2ef2c7df3599ee1aab45641ce51b2b26151f7da0
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-03-23 15:08:10 -0400

    tweaked param name

--- src/create-sns-sqs-subscription.ts
+++ src/create-sns-sqs-subscription.ts
@@ -2,14 +2,14 @@ import { Construct } from 'constructs';
 import * as sns from 'aws-cdk-lib/aws-sns';
 
 export class CreateSnsSqsSubscription extends Construct {
-    constructor(scope: Construct, id: string, topicArn: string, filterPolicy: any, endpoint: string) {
+    constructor(scope: Construct, id: string, topicArn: string, filterPolicy: any, endpointArn: string) {
         super(scope, id);
 
         new sns.CfnSubscription(this, 'SNSSubscription', {
             topicArn: topicArn,
             filterPolicy: filterPolicy,
             filterPolicyScope: 'MessageBody',
-            endpoint: endpoint,
+            endpoint: endpointArn,
             protocol: 'sqs',
             rawMessageDelivery: false,
             region: 'us-east-1',
